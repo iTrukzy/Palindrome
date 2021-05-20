d3104dafd32529f8a4b48d724b7095b3b238f2d9
iTrukzy <74843848+iTrukzy@users.noreply.github.com>
2021-05-20 11:53:31 -0500

    updated to all palindromes

--- js.js
+++ js.js
@@ -2,36 +2,29 @@ let string =
   "anulalalunapaisajemontanaguaamoraromacomidaluzazulsillagatobotellacamarayosoypalindromocasaverdebanderaventanacangrejolarutanosaportootropasonaturaliniciaracaestoseralodoodolaresdonasbarcosmarcieloaviontierrapaisbicicletaestonoespalindromojugarseverlasalrevesusandounradarenelojorejero";
 
 const revPalindrome = (string) => {
-  let reverse = string.split("").reverse().join("")
-  return string == reverse
-}
+  let reverse = string.split("").reverse().join("");
+  return string == reverse;
+};
 
 
 const palindrome = (string) => {
-let length = 0;
-let result = '';
+  let length = 3;
+  let result = [];
 
-for(let i = 0; i < string.length; i++) {
-  let catn = string.substr(i, string.length);
+  for (let i = 0; i < string.length; i++) {
+    let catn = string.substr(i, string.length);
 
-  for(let j = catn.length; j >= 0; j--) {
-    let SubsString = catn.substr(0, j);
-    if(SubsString.length <= 1)
-    continue;
+    for (let j = catn.length; j >= 0; j--) {
+      let SubsString = catn.substr(0, j);
 
-    if(revPalindrome(SubsString)) {
-      if(SubsString.length > length) {
-        length = SubsString.length;
-        result = SubsString
+      if (revPalindrome(SubsString)) {
+        if (SubsString.length > length) {
+          result.push(SubsString);
+        }
       }
     }
   }
-}
-return result
-}
+  return result;
+};
 
-console.log(palindrome(string))
-
-
-
-
+console.log(palindrome(string));
